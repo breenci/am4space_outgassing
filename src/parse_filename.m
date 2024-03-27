56719e241b0b46ae846d35ae6914b5c573996d00
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2024-03-27 12:28:27 +0000

    Add function to extract info from filename
    Function extracts measurement info from filename in format given in
    README.
    
    Will currently only work on sample measurement files not references

---
+++ src/parse_filename.m
@@ -0,0 +1,22 @@
+function fn_struct = parse_filename(fn)
+% PARSE_FILENAME Extract measurement information from filename
+%   fn_struct = PARSE_FILENAME(fn) extracts info from filename string and
+%   returns a structure cotaining the relevent information
+
+% split the filename to extract information
+info_arr = split(fn, '_');
+
+% create the datetime object
+datetime_str = strcat(info_arr(1), info_arr(2));
+datetime_fmt = 'yyyyMMddHHmmss';
+meas_datetime = datetime(datetime_str, "Format", datetime_fmt);
+
+% create a structure for the sample info
+sample_info = split(info_arr(3), '-');
+sample_struct = struct('name', sample_info(1), 'id', ...
+    str2double(sample_info(2)), 'shape', sample_info(3));
+
+% create a structure to store all of the information
+fn_struct = struct('datetime', meas_datetime, 'operator', info_arr(4), ...
+    'sample', sample_struct);
+end
